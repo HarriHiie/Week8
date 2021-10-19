56d16b22d89dc6ae0c9ec2e7d7b7ab2393a69407
Opilane TTHK <opilane@tthk.ee>
2021-10-19 11:30:14 +0300

    function

---
+++ functions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta oma nimi");
+            string Username = Console.ReadLine();
+            
+            
+            sayHello(Username);
+
+        }
+
+        private static void sayHello(string somename)
+        {
+            Console.WriteLine($"Hi There! {somename}");
+        }
+    }
+}
